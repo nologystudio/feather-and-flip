7da16fb9b56f6099466881b67ad8aea3447e844e
Pepe López <pepe@nologystudio.com>
2014-10-10 18:38:30 +0100

    Expedia and Sabre REST connections are ready

---
+++ sites/all/modules/expedia_services/classes/Expedia.class.php
@@ -0,0 +1,43 @@
+<?php
+
+class Expedia
+{
+	/*
+	*	Get holtel list for city name, country code
+	*	@param city
+	* 	@param countryCode
+	*/
+	public static function CityHotels($city, $country = '')
+	{
+		$service = wsclient_service_load('expedia__rest');
+		$service->settings['http_headers'] = array(
+			'Content-Type' => array('multipart/form-data'),
+		);
+
+
+		$service->settings['curl options'] = array(
+			CURLOPT_POSTFIELDS => array(
+				'city' => $city,
+				'countryCode' => $country,
+				'numberOfResults' => 12)
+		);
+
+		$res = null;
+		try {
+			$res = $service->expedia__rest_hotel_list();
+		} catch (Exception $e) {
+			return $e->getMessage();
+		}
+		return $res;
+	}
+
+	/*
+	*	Extract EAN error message
+	*	@param Expedia object
+	* 	
+	*/
+	public static function GetErrorMessage($obj)
+	{
+		return $obj['HotelListResponse']['EanWsError']['presentationMessage'];
+	}
+}
