f68bd553715a7b6ad299266637388ec66e3ffe96
Francisco Fonseca <francisco@nologystudio.com>
2014-10-21 19:03:09 +0100

    Front-End integration

---
+++ sites/all/themes/feflip/templates/views-view--home--page.tpl.php
@@ -0,0 +1,77 @@
+<section id="start-your-journey">
+        <header>start your journey</header>
+        <div class="gallery-wrapper">
+                <?php for($i=0;$i<5;$i++): ?>
+                <a href="" rel="destination" class="gallery-item">
+                        <figure class="circle-mask">
+                                <img src="" alt=""/>
+                        </figure>
+                        <h2>destination</h2>
+                </a>
+                <?php endfor; ?>
+        </div>
+</section>  
+<section id="map-it">
+        <header>
+                <img src="" alt="MAP IT. Where to go now"/>
+        </header>
+        <aside>
+                <nav>
+                        <ul>
+                                <li data-image=""><span>hotels</span>destination</li>
+                                <li data-image=""><span>0</span>destination</li>
+                                <li data-image=""><span>1</span>destination</li>
+                                <li data-image=""><span>2</span>destination</li>
+                                <li data-image=""><span>3</span>destination</li>
+                        </ul>
+                        <figure>
+                                <img src="" alt=""/>
+                        </figure>
+                </nav>
+        </aside>
+        <div id="map">
+                <div class="pin" data-lat="" data-lon="">
+                        <a href="" class="info">
+                                <div class="wrapper">
+                                        <figure>
+                                                <img src="" alt=""/>
+                                        </figure>
+                                        <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English</p>
+                                </div>
+                        </a>
+                        <small>destination</small>
+                </div>
+        </div>
+</section>
+<section id="blog">
+        <header>
+                <button class="rounded-btn icon arrow-down">Filter by category</button>
+                <ul>
+                        <li>category</li>
+                        <li>category</li>
+                        <li>category</li>
+                </ul>
+        </header>
+        <div class="quick-entry">
+                <h3>Join the adventure</h3>
+                <h4>Sign up for our newsletter</h4>
+                <form id="newsletter-signup">
+                        <input type="email" placeholder="" value=""/>
+                        <input type="submit" value="submit"/>
+                </form>
+        </div>
+        <article class="quick-entry">
+                <figure>
+                        <img src="" alt=""/>
+                </figure>
+                <footer>
+                        <h4>Block title</h4>
+                        <time datetime="2008-02-14 20:00">Date</time>
+                </footer>
+        </article>
+        <article class="quick-entry">
+                <h3>hotel le panam</h3>
+                <h4>Sign up for our newsletter</h4>
+                <time datetime="2008-02-14 20:00">Date</time>
+        </article>
+</section>   
