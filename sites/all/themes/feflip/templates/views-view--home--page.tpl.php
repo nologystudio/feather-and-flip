b25d5d5c4c02e1d6b5caf45f560651e9af42628d
Francisco Fonseca <francisco@nologystudio.com>
2014-11-10 13:46:05 +0000

    Integration with mail chimp

--- sites/all/themes/feflip/templates/views-view--home--page.tpl.php
+++ sites/all/themes/feflip/templates/views-view--home--page.tpl.php
@@ -59,10 +59,10 @@
                         <h3>Join the adventure</h3>
                         <hr>
                         <h4>Sign up for our newsletter</h4>
-                        <form>
-                                <input type="email" placeholder="Your email address" value=""/>
+                        <form id="signupNewsLetter" method="POST">
+                                <input id="user-email" type="email" placeholder="Your email address" value=""/>                
                                 <input type="submit" value="submit"/>
-                        </form>
+                        </form>  
                 </div>
                 <?php for($i=0;$i<5;$i++): ?>
                 <article class="quick-entry">
