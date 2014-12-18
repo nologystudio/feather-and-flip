d8905b8e8a557ca3e1f69c468b19f729d2f8e76f
Pepe López <joselopezguerra@gmail.com>
2014-12-18 16:49:32 +0000

    Main slideshow link control

--- sites/all/themes/feflip/templates/slideshowandmainmenu.html.php
+++ sites/all/themes/feflip/templates/slideshowandmainmenu.html.php
@@ -7,23 +7,37 @@
                                 <figure>
                                         <img src="<?php echo $image['url'];?>" alt="" data-size="<?php echo $image['size'][0].'x'.$image['size'][1];?>"/>
                                 </figure>
-                                <div class="info-wrapper animated fadeInUp">
+                                <div class="info-wrapper animated fadeInUp<?php echo (!isset($image['btntext']) ? ' static' : ''); ?>">
+                                    <?php if (isset($image['linkto'])) { ?>
                                         <a href="<?php if(isset($image['linkto'])) echo $image['linkto']; else echo ''; ?>" <?php if(isset($blank) && $blank){ echo 'target="_blank"'; } ?> rel="">
-                                                <h1><?php if(isset($image['text'])) echo $image['text']; else echo 'city, country'; ?></h1>
-                                                <?php if(isset($image['subtitle'])){ ?>
-                                                <span class="cursive"><?php echo $image['subtitle']; ?></span>
-                                                <?php } ?>
-                                                <?php if(isset($image['btntext'])){?>
-                                                <span class="rounded-btn"><?php echo $image['btntext'];?></span>
-                                                <?php } ?>
+                                            <h1><?php if(isset($image['text'])) echo $image['text']; else echo 'city, country'; ?></h1>
+                                            <?php if(isset($image['subtitle'])){ ?>
+                                            <span class="cursive"><?php echo $image['subtitle']; ?></span>
+                                            <?php } ?>
+                                            <?php if(isset($image['btntext'])){?>
+                                            <span class="rounded-btn"><?php echo $image['btntext'];?></span>
+                                            <?php } ?>
                                         </a>
+                                    <?php } else { ?>
+                                        <div>
+                                            <h1><?php if(isset($image['text'])) echo $image['text']; else echo 'city, country'; ?></h1>
+                                            <?php if(isset($image['subtitle'])){ ?>
+                                            <span class="cursive"><?php echo $image['subtitle']; ?></span>
+                                            <?php } ?>
+                                            <?php if(isset($image['btntext'])){?>
+                                            <span class="rounded-btn"><?php echo $image['btntext'];?></span>
+                                            <?php } ?>
+                                        </div>
+                                    <?php } ?>
                                 </div>
                         </article>
                      </li>
                     <?php } ?>
                </ul>
-            <button rel="left"></button>
-            <button rel="right"></button>
+            <?php if (count($slideImages) > 1) { ?>
+                <button rel="left"></button>
+                <button rel="right"></button>
+            <?php } ?>
         </div>
                 
         <?php 
