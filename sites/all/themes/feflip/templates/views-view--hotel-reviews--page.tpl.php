17f60a059d9aee5b398aa796c4307f3825806107
Francisco Fonseca <francisco@nologystudio.com>
2014-12-22 22:45:48 +0000

    Rearrange hotel results by availability

--- sites/all/themes/feflip/templates/views-view--hotel-reviews--page.tpl.php
+++ sites/all/themes/feflip/templates/views-view--hotel-reviews--page.tpl.php
@@ -42,72 +42,89 @@
 					</ul>
 				</div>
 				</ul>
-                <?php foreach($hotels as $hotel): ?>
-                <?php
-                        $rates = Hotel::GetResponseRates($hotelRates, $hotel['sabreCode'], $hotel['expediaCode']);
-                        $service = '';
-                        if (((float)$rates['expedia']['rate'] != 0.0) && ((float)$rates['sabre']['rate'] != 0.0))
-                        {
-                            if ((float)$rates['expedia']['rate']  < (float)$rates['sabre']['rate'])
-                            {
-                                $service = 'expedia';
-                                $serviceCode = 'expediaCode';
-                                $rate = (float)$rates['expedia']['rate'];
-                                $curr = $rates['expedia']['currency'];
-                            }
-                            else
-                            {
-                                $service = 'sabre';
-                                $serviceCode = 'sabreCode';
-                                $rate = (float)$rates['sabre']['rate'];
-                                $curr = $rates['sabre']['currency'];
-                            }
-                        }
-                        elseif(((float)$rates['expedia']['rate'] == 0.0) && ((float)$rates['sabre']['rate'] != 0.0))
-                        {
-                            $service = 'sabre';
-                            $serviceCode = 'sabreCode';
-                            $rate = (float)$rates['sabre']['rate'];
-                            $curr = $rates['sabre']['currency'];
-                        }
-                        elseif(((float)$rates['expedia']['rate'] != 0.0) && ((float)$rates['sabre']['rate'] == 0.0))
+            <?php
+            if ($showPrice)
+            {
+                $newHotels = array();
+
+                foreach($hotels as $hotel)
+                {
+                    $rates = Hotel::GetResponseRates($hotelRates, $hotel['sabreCode'], $hotel['expediaCode']);
+                    $service = '';
+                    if (((float)$rates['expedia']['rate'] != 0.0) && ((float)$rates['sabre']['rate'] != 0.0))
+                    {
+                        if ((float)$rates['expedia']['rate']  < (float)$rates['sabre']['rate'])
                         {
                             $service = 'expedia';
                             $serviceCode = 'expediaCode';
                             $rate = (float)$rates['expedia']['rate'];
                             $curr = $rates['expedia']['currency'];
                         }
+                        else
+                        {
+                            $service = 'sabre';
+                            $serviceCode = 'sabreCode';
+                            $rate = (float)$rates['sabre']['rate'];
+                            $curr = $rates['sabre']['currency'];
+                        }
+                    }
+                    elseif(((float)$rates['expedia']['rate'] == 0.0) && ((float)$rates['sabre']['rate'] != 0.0))
+                    {
+                        $service = 'sabre';
+                        $serviceCode = 'sabreCode';
+                        $rate = (float)$rates['sabre']['rate'];
+                        $curr = $rates['sabre']['currency'];
+                    }
+                    elseif(((float)$rates['expedia']['rate'] != 0.0) && ((float)$rates['sabre']['rate'] == 0.0))
+                    {
+                        $service = 'expedia';
+                        $serviceCode = 'expediaCode';
+                        $rate = (float)$rates['expedia']['rate'];
+                        $curr = $rates['expedia']['currency'];
+                    }
 
-                        // hotel categories classes
-                        $hClasses = implode(' ', $hotel['categories']);
-                ?>
+                    if (!empty($service)) {
+                        $hotel['service'] = $service;
+                        $hotel['serviceCode'] = $serviceCode;
+                        $hotel['rate'] = $rate;
+                        $hotel['curr'] = $curr;
+                        array_unshift($newHotels, $hotel);
+                    }
+                    else
+                        array_push($newHotels, $hotel);
 
-                         <a class="item<?php echo (!empty($hClasses) ? ' '.$hClasses : ''); ?>" href="<?php echo $hotel['url']; ?>"<?php echo (!empty($service) ? ' data-service="'.$service.'"' : ''); ?><?php echo (!empty($service) ? ' data-hotelId="'.$hotel[$serviceCode].'"' : ''); ?> data-internalId="<?php echo $hotel['id'] ?>">
-                            <figure>
-                                    <img src="<?php echo $hotel['image'];?>" alt=""/>
-                            </figure>
-                            <div id="hotel-name">
-                                <h2><?php echo $hotel['name'];?></h2>
-                            </div>
-							<div id="hotel-destination">
-                                <h3><?php echo $hotel['destination'];?></h3>
-							</div>
-                            <?php if($showPrice){?>
-                            <?php if (!empty($service)) { ?>
-                                    <button rel="booking" class="animated fadeInUp">
-                                            <span>starting from</span>
-                                            <h4><?php echo $rate; ?> <?php echo $curr; ?></h4>
-                                    </button>
-                            <?php } else { ?>
-                                    <button rel="booking" class="warning animated fadeInUp">
-                                            <span>not</span>
-                                            <h4>available</h4>
-                                    </button>
-                            <?php } }?>
-                        </a>
-                <?php endforeach; ?>
+                }
+
+                $hotels = $newHotels;
+            }
+            ?>
+            <?php foreach($hotels as $hotel){ $hClasses = implode(' ', $hotel['categories']);?>
+                <a class="item<?php echo (!empty($hClasses) ? ' '.$hClasses : ''); ?>" href="<?php echo $hotel['url']; ?>"<?php echo (!empty($hotel['service']) ? ' data-service="'.$hotel['service'].'"' : ''); ?><?php echo (!empty($hotel['service']) ? ' data-hotelId="'.$hotel[$serviceCode].'"' : ''); ?> data-internalId="<?php echo $hotel['id'] ?>">
+                    <figure>
+                        <img src="<?php echo $hotel['image'];?>" alt=""/>
+                    </figure>
+                    <div id="hotel-name">
+                        <h2><?php echo $hotel['name'];?></h2>
+                    </div>
+                    <div id="hotel-destination">
+                        <h3><?php echo $hotel['destination'];?></h3>
+                    </div>
+                    <?php if($showPrice){?>
+                        <?php if (!empty($hotel['service'])) { ?>
+                            <button rel="booking" class="animated fadeInUp">
+                                <span>starting from</span>
+                                <h4><?php echo $hotel['rate']; ?> <?php echo $hotel['curr']; ?></h4>
+                            </button>
+                        <?php } else { ?>
+                            <button rel="booking" class="warning animated fadeInUp">
+                                <span>not</span>
+                                <h4>available</h4>
+                            </button>
+                        <?php } }?>
+                </a>
+            <?php } ?>
         </div>
-        <footer>
-                <button rel="load-more">load more</button>
-        </footer>
+    <footer>
+        <button rel="load-more">load more</button>
+    </footer>
 </section>
