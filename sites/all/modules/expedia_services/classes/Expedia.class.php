859e8eaa636d9f2568f0ebfe3d7b31d0ac5a3679
Francisco Fonseca <francisco@nologystudio.com>
2014-12-23 15:52:04 +0000

    Pruebas de booking en expedia

--- sites/all/modules/expedia_services/classes/Expedia.class.php
+++ sites/all/modules/expedia_services/classes/Expedia.class.php
@@ -155,20 +155,73 @@ class Expedia
         $service->settings['http_headers'] = array(
             //'Content-Type' => array('multipart/form-data'),
             'Content-Type' => array('application/x-www-form-urlencoded'),
+            //'Accept' => array('application/xml')
         );
-
+        $service->url = 'https://book.api.ean.com';
+        $xml = <<<SESSIONCREATEXML
+ <HotelRoomReservationRequest>
+    <hotelId>106347</hotelId>
+    <arrivalDate>1/22/2015</arrivalDate>
+    <departureDate>1/24/2015</departureDate>
+    <supplierType>E</supplierType>
+    <rateKey>af00b688-acf4-409e-8bdc-fcfc3d1cb80c</rateKey>
+    <roomTypeCode>198058</roomTypeCode>
+    <rateCode>484072</rateCode>
+    <chargeableRate>231.18</chargeableRate>
+    <RoomGroup>
+        <Room>
+            <numberOfAdults>2</numberOfAdults>
+            <firstName>test</firstName>
+            <lastName>tester</lastName>
+            <bedTypeId>23</bedTypeId>
+            <smokingPreference>NS</smokingPreference>
+        </Room>
+    </RoomGroup>
+    <ReservationInfo>
+        <email>test@travelnow.com</email>
+        <firstName>test</firstName>
+        <lastName>tester</lastName>
+        <homePhone>2145370159</homePhone>
+        <workPhone>2145370159</workPhone>
+        <creditCardType>CA</creditCardType>
+        <creditCardNumber>5401999999999999</creditCardNumber>
+        <creditCardIdentifier>123</creditCardIdentifier>
+        <creditCardExpirationMonth>11</creditCardExpirationMonth>
+        <creditCardExpirationYear>2016</creditCardExpirationYear>
+    </ReservationInfo>
+    <AddressInfo>
+        <address1>travelnow</address1>
+        <city>Seattle</city>
+        <stateProvinceCode>WA</stateProvinceCode>
+        <countryCode>US</countryCode>
+        <postalCode>98004</postalCode>
+    </AddressInfo>
+</HotelRoomReservationRequest>
+SESSIONCREATEXML;
         // Set roomGroup data
         foreach ($roomConfig as $room) {
-            $roomGroup[] = array(
+            $values = array(
                 'Room' => array(
                     'numberOfAdults' => $room['adults'],
-                    'numberOfChildren' => (isset($room['children']) && ($room['children']['number'] > 0)) ? count($room['children']['ages']) : 0,
-                    'childAges' => (isset($room['children']) && ($room['children']['number'] > 0)) ? implode(',', $room['children']['ages']) : '',
+                    //'numberOfChildren' => (isset($room['children']) && ($room['children']['number'] > 0)) ? count($room['children']['ages']) : 0,
+                    //'childAges' => (isset($room['children']) && ($room['children']['number'] > 0)) ? implode(',', $room['children']['ages']) : '',
                     'firstName' => $firstname,
                     'lastName' => $lastname,
+                    'bedTypeId'=> '15',
+                    'smokingPreference' =>'NS'
                 )
             );
+
+            if(isset($room['children']) && ($room['children']['number'] > 0))
+            {
+                $values['room']['numberOfChildren'] = count($room['children']['ages']);
+                $values['room']['childAges'] = implode(',', $room['children']['ages']);
+            }
+
+            $roomGroup[] = $values;
         }
+
+
 
         // Set ReservationInfo data
         $reservationInfo = array(
@@ -183,22 +236,53 @@ class Expedia
             'creditCardExpirationYear'  => $creditCardExpirationYear
         );
 
+
+
+        //AddressInfo
+        $addressInfo = array(
+            'address1' => 'travelnow',
+            'city' => 'Seattle',
+            'stateProvinceCode' => 'WA',
+            'countryCode' => 'US',
+            'postalCode' => '98004'
+        );
+
+
+
         $res = null;
 
+        /*
         $service->settings['curl options'] = array(
             CURLOPT_POSTFIELDS => array(
+                //'xml' => htmlspecialchars($xml)
                 'hotelId' => $hotelId,
                 'arrivalDate' => $checkin,
                 'departureDate' => $checkout,
                 'roomTypeCode' => $roomcode,
                 'rateCode' => $ratecode,
                 'RoomGroup' => json_encode($roomGroup),
-                'ReservationInfo' => /*json_encode($reservationInfo)*/ implode(",",$reservationInfo))
+                'ReservationInfo' => json_encode($reservationInfo),
+                'AddressInfo' => json_encode($addressInfo),
+                'supplierType' => 'E',
+                'rateKey' => '9dea54b4-2b95-4346-9ae1-3185757a96e9'
+        ));
+        */
+
+        /*
+        $service->settings['curl options'] = array(
+            CURLOPT_SSL_VERIFYPEER => false
         );
+        */
+
+        /*
+        $service->settings['curl options'] = array(
+            CURLOPT_SSL_VERIFYPEER => false,
+            CURLOPT_SSL_VERIFYHOST => 2
+        );*/
 
         try{
-            //$res = $service->expedia__rest_room_reservation($hotelId, $checkin, $checkout, $roomcode, $ratecode, json_encode($roomGroup), json_encode($reservationInfo));
-            $res = $service->expedia__rest_room_reservation();
+            //$res = $service->expedia__rest_room_reservation($hotelId, $checkin, $checkout, $roomcode, $ratecode, json_encode($roomGroup), json_encode($reservationInfo), json_encode($addressInfo),'E', '9dea54b4-2b95-4346-9ae1-3185757a96e9');
+            $res = $service->expedia__rest_room_reservation_xml($xml);
         }
         catch(Exception $e){
             return $e->getMessage();
