27bc368e62cc34167248df62385c42dfcb476eae
raulanatol <raulanatol@deportunity.com>
2015-05-07 15:31:35 +0100

    Environment script configuration.

--- sites/all/modules/feflip_features/classes/Env.class.php
+++ sites/all/modules/feflip_features/classes/Env.class.php
@@ -5,5 +5,6 @@ class Env {
   const MAILCHIMP_API_KEY = 'eae1d5448b1a2cb751661162fd5011fd-us8';
   const MAILCHIMP_LIST_ID = '487e37ac3f';
 
+  const OTTO_BASE_PATH = "http://otto-env.elasticbeanstalk.com/pre";
 
 }
