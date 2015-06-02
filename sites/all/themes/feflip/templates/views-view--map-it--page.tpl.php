467018cc85b74d30ea1b286dd04b52c33e1ec0ba
Pepe López <pepe@nologystudio.com>
2015-06-02 01:29:17 +0100

    CityGuide functionality - Desktop & Mobile fix

--- sites/all/themes/feflip/templates/views-view--map-it--page.tpl.php
+++ sites/all/themes/feflip/templates/views-view--map-it--page.tpl.php
@@ -1,6 +1,6 @@
 <?php include 'slideshowandmainmenu.html.php';?>
 
-<section id="map-it" class="full" ng-controller="FullMapCtrl" data-origin="30">
+<section id="map-it" class="full" ng-controller="FullMapCtrl">
     <header class="animated fadeInUp">
             <h3 class="icon compass">MAP IT</h3>
             <nav id="zoom">
