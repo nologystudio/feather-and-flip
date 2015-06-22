95b2bc9e60c1159d615f8878b4e694629ad5afce
Pepe López <pepe@nologystudio.com>
2015-06-22 19:02:14 +0100

    Fix mobile/desktop redirects

--- index.php
+++ index.php
@@ -25,11 +25,11 @@ drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
 $detect = mobile_detect_get_object();
 $is_mobile = $detect->isMobile();
 if ($is_mobile && $_SERVER['HTTP_HOST'] != 'm.featherandflip.com') {
-  header('Location: https://m.featherandflip.com');
+  header('Location: https://m.featherandflip.com'.$_SERVER['REQUEST_URI']);
   exit();
 }
 if (!$is_mobile && $_SERVER['HTTP_HOST'] == 'm.featherandflip.com') {
-  header('Location: https://www.featherandflip.com');
+  header('Location: https://www.featherandflip.com'.$_SERVER['REQUEST_URI']);
   exit();
 }
 
