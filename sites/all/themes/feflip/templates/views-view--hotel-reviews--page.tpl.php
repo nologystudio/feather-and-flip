b068c60934d37675cfd3564738939832e98a1e7c
Aday <aday@nologystudio.com>
2015-06-24 02:40:34 +0100

    New Implementation For Travel Journal & City Guide
    + CSS Style
    + HTML mark-up

--- sites/all/themes/feflip/templates/views-view--hotel-reviews--page.tpl.php
+++ sites/all/themes/feflip/templates/views-view--hotel-reviews--page.tpl.php
@@ -115,94 +115,60 @@
 <?php } ?>
 <!-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  - - - - - - - - - -  -->
 <section id="hotel-reviews"<?php echo ((isset($arg[2]) && ($arg[2] == 'itinerary')) ? ' class="hidden"' : ''); ?>>
-		<!--<header id="booking-header-engine">
-				
-            <!--<?php if(count($inputValues) > 0){?>
-                <form id="booking-search" ng-controller="BookingEngineCtrl" ng-include="searchTpl" ng-init='init(<?php echo json_encode($inputValues);?>)'></form>
-            <?php } else { ?>
-                <form id="booking-search" ng-controller="BookingEngineCtrl" ng-include="searchTpl" ng-init="bookingInfo.destination = <?php if(isset($destinationId)) echo $destinationId; else echo 0;?>"></form>
-            <?php } ?>
-		</header>-->
-        <?php if($showNotAvailable){?>
+	<?php if($showNotAvailable){?>
 		<div role="header" class="not-available">
 			<div>
 				<h6>Don't see what you're looking for? Online availability isn't always accurate. <a href="/contact">Contact our team</a> and we'll check with the hotel directly for you.</h6>
 			</div>
 		</div>
-        <?php }?>
-        <div class="wrapper">
-                <h2 class="middle-line"><?php echo $destinationDescription;?></h2>
-                <div id="filter-container">
-					<ul role="select" class="dark">
-						<li>Reset Filters</li>
-					</ul>
-					<ul id="filter-list" ng-controller="HotelFilterCtrl">
-						<?php
-	                        $name = 'hoteltags';
-	                        $myvoc = taxonomy_vocabulary_machine_name_load($name);
-	                        $tree = taxonomy_get_tree($myvoc->vid);
-	                        foreach ($tree as $term) {
-	                            echo '<li><span></span>'.$term->name.'</li>';
-	                        }
-	                    ?>
-					</ul>
-				</div>
-				</ul>
-            <?php foreach($hotels as $hotel){ $hClasses = implode(' ', $hotel['categories']);?>
-                <a class="item<?php echo (!empty($hClasses) ? ' '.$hClasses : ''); ?>" href="<?php echo $hotel['url']; ?>"<?php echo (!empty($hotel['_service']) ? ' data-service="'.$hotel['_service'].'"' : ''); ?><?php echo (!empty($hotel['_service']) ? ' data-hotelId="'.$hotel[$hotel['_serviceCode']].'"' : ''); ?> data-internalId="<?php echo $hotel['id'] ?>"
-                    <?php if (isset($hotel['expedia_rate'])){echo 'data-expedia = "'.$hotel['expedia_rate'] .' '. $hotel['expedia_curr'].'|' . $hotel['expediaCode'].'" ';} if (isset($hotel['sabre_rate'])){echo 'data-sabre = "'.$hotel['sabre_rate'] .' '. $hotel['sabre_curr'].'|' . $hotel['sabreCode'] .'"';}?>>
-                    <figure>
-                        <img src="<?php echo $hotel['image'];?>" alt=""/>
-                    </figure>
-                    <div id="hotel-name">
-                        <h2><?php echo $hotel['name'];?></h2>
-                    </div>
-                    <div id="hotel-destination">
-                        <h3><?php echo $hotel['destination'];?></h3>
-                    </div>
-                    <?php if($showPrice){?>
-                        <?php if (!empty($hotel['_service'])) { ?>
-                            <button rel="booking" class="animated fadeInUp">
-                                <span>starting from</span>
-                                <h4><?php echo $hotel['_rate']; ?> <?php echo $hotel['_curr']; ?></h4>
-                            </button>
-                        <?php } else { ?>
-                            <button rel="booking" class="warning animated fadeInUp">
-                                <span>not</span>
-                                <h4>available</h4>
-                            </button>
-                        <?php } } else {?>
-                        	<h4 class="description"><?php echo $hotel['hotelDescription'];?></h4>
-                        <?php } ?>
-                </a>
-            <?php } ?>
-        </div>
-    <footer>
-        <button rel="load-more">load more</button>
-    </footer>
+    <?php }?>
+    <div class="wrapper">
+        <h2 class="middle-line"><?php echo $destinationDescription;?></h2>
+        <div id="filter-container">
+			<ul role="select" class="dark">
+				<li>Reset Filters</li>
+			</ul>
+			<ul id="filter-list" ng-controller="HotelFilterCtrl">
+				<?php
+                    $name = 'hoteltags';
+                    $myvoc = taxonomy_vocabulary_machine_name_load($name);
+                    $tree = taxonomy_get_tree($myvoc->vid);
+                    foreach ($tree as $term) {
+                        echo '<li><span></span>'.$term->name.'</li>';
+                    }
+                ?>
+			</ul>
+		</div>
+		<?php foreach($hotels as $hotel){ $hClasses = implode(' ', $hotel['categories']);?>
+            <a class="item<?php echo (!empty($hClasses) ? ' '.$hClasses : ''); ?>" href="<?php echo $hotel['url']; ?>"<?php echo (!empty($hotel['_service']) ? ' data-service="'.$hotel['_service'].'"' : ''); ?><?php echo (!empty($hotel['_service']) ? ' data-hotelId="'.$hotel[$hotel['_serviceCode']].'"' : ''); ?> data-internalId="<?php echo $hotel['id'] ?>"
+            <?php if (isset($hotel['expedia_rate'])){echo 'data-expedia = "'.$hotel['expedia_rate'] .' '. $hotel['expedia_curr'].'|' . $hotel['expediaCode'].'" ';} if (isset($hotel['sabre_rate'])){echo 'data-sabre = "'.$hotel['sabre_rate'] .' '. $hotel['sabre_curr'].'|' . $hotel['sabreCode'] .'"';}?>>
+                <figure>
+                    <img src="<?php echo $hotel['image'];?>" alt=""/>
+                </figure>
+                <div id="hotel-name">
+                    <h2><?php echo $hotel['name'];?></h2>
+                </div>
+                <div id="hotel-destination">
+                    <h3><?php echo $hotel['destination'];?></h3>
+                </div>
+                
+                <?php if($showPrice){?>
+                    <?php if (!empty($hotel['_service'])) { ?>
+                        <button rel="booking" class="animated fadeInUp">
+                            <span>starting from</span>
+                            <h4><?php echo $hotel['_rate']; ?> <?php echo $hotel['_curr']; ?></h4>
+                        </button>
+                    <?php } else { ?>
+                        <button rel="booking" class="warning animated fadeInUp">
+                            <span>not</span>
+                            <h4>available</h4>
+                        </button>
+                    <?php } } else {?>
+                    <h4 class="description"><?php echo $hotel['hotelDescription'];?></h4>
+                <?php } ?>
+            </a>
+        <?php } ?>
+    </div>
+    <div class="city-guide-block"><a href="">View our city guide for...</a></div>
 </section>
 <?php if (isset($travel_journal) && !empty($travel_journal)) print $travel_journal; ?>
-<div class="service-overlay">
-			<div class="content">
-				<button rel="close"></button>
-				<header>BOOK NOW!</header>
-				<div id="expedia-service">
-					<button>
-						<span>starting from</span>
-						<h4>1000$</h4>
-					</button>
-					<small>book with expedia - online only</small>
-				</div>
-				<div id="sabre-service">
-					<button>
-						<span>starting from</span>
-						<h4>1000$</h4>
-					</button>
-					<small>*book with feather+flip</small>
-				</div>
-				<footer>
-					<sup>*</sup>
-					Feather+Flip perks may include free breakfasts, room upgrades and spa credits at partner hotels.
-				</footer>
-			</div>
-		</div>
