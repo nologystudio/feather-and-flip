11e636945c2d25d149402ab4d33fcfe0eb29617f
Pepe López <pepe@nologystudio.com>
2015-08-10 15:28:24 +0100

    [ back-end ] Unifying cookie domain.

--- .ebextensions/env/PRO/settings.php
+++ .ebextensions/env/PRO/settings.php
@@ -394,7 +394,7 @@ ini_set('session.cookie_lifetime', 2000000);
  * between your various domains. Make sure to always start the $cookie_domain
  * with a leading dot, as per RFC 2109.
  */
-$cookie_domain = 'www.passported.com';
+$cookie_domain = '.passported.com';
 
 /**
  * Variable overrides:
