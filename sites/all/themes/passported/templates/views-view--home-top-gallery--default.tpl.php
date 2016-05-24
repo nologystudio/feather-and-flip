6134075c21d4c6819ba05b17b4100300ec5d5882
Pepe López <pepe@nologystudio.com>
2016-05-24 01:31:17 +0100

    New homepage layout and some statics. Drupal views.

---
+++ sites/all/themes/passported/templates/views-view--home-top-gallery--default.tpl.php
@@ -0,0 +1,40 @@
+<div class="gallery-wrapper">
+    <?php if (isset($view->result) && !empty($view->result)): ?>
+        <ul>
+            <?php foreach ($view->result as $post) {
+                $orig_date = strtotime($post->field_field_original_pubdate[0]['raw']['safe_value']);
+                $post_url = $post->field_field_original_url[0]['raw']['safe_value'];
+                $img_url = image_style_url('home_top_gallery', $post->field_field_image[0]['raw']['uri']); ?>
+                <li>
+                    <a href="<?php echo $post_url; ?>" target="_blank" class="blog-entry">
+                        <figure>
+                            <img src="<?php echo $img_url; ?>" alt="<?php echo $post->node_title; ?>"/>
+                        </figure>
+                        <footer>
+                            <h2><?php echo $post->node_title; ?></h2>
+                            <time><?php echo date('F, Y', $orig_date); ?></time>
+                        </footer>
+                    </a>
+                </li>
+            <?php } ?>
+        </ul>
+    <?php endif; ?>
+    <button rel="left">
+        <svg width="40" height="80" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+            <defs>
+                <g id="left-arrow"><path d="M 0,0 L 0,0 L 40,40 L 0,80"></path></g>
+            </defs>
+            <use x="0" y="0" xlink:href="#left-arrow"/>
+        </svg>
+        <span></span>
+    </button>
+    <button rel="right">
+        <svg width="40" height="80" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+            <defs>
+                <g id="right-arrow"><path d="M 40,0 L 40,0 L 0,40 L 40,80"></path></g>
+            </defs>
+            <use x="0" y="0" xlink:href="#right-arrow"/>
+        </svg>
+        <span></span>
+    </button>
+</div>
--- sites/all/themes/passported/templates/views-view--new-home--page.tpl.php
+++ sites/all/themes/passported/templates/views-view--new-home--page.tpl.php
@@ -1,88 +1,83 @@
 	
 	<!-- Main Block -->
 		
-	<main id="passported-intro">
-		<ul>
-			<li>
-				<h1 data-animate="1">
-					travel simplified</h1>
-				</h1>
-				<ul class="align-center">
-					<li ng-click="goToURL('city-guides')">
-						<h2 data-animate="4">Explore</h2>
-						<a href="/city-guides"><h3 data-animate="5">Explore our city guides</h3></a>
-					</li>
-					<li ng-click="goToURL('https://go.passported.com')">
-						<h2 data-animate="2">Plan</h2>
-						<a href="https://go.passported.com"><h3 data-animate="3">Create your itinerary</h3></a>
-					</li>
-					<li ng-click="goToURL('book-hotels')">
-						<h2 data-animate="6">Book</h2>
-						<a href="/book-hotels"><h3 data-animate="7">Email our hotel expert</h3></a>
-					</li>
-				</ul>
-			</li>
-		</ul>
+	<main id="passported-home" class="full">
+        <?php if (isset($top_gallery)) echo $top_gallery; ?>
 	</main>
-	
-	<!-- inspiration -->
-	
-	<section id="inspiration">
-		<div class="wrapper grid-2 align-center" ng-controller="InspirationController">
-			<div id="let-us-inspire" data-animate="1">
-				<header>
-					<h4>Find Your Perfect Trip</h4>
-				</header>
-				<div class="wrapper">
-					<div pp-inspiration-select id="place-select" class="select" data-options="adventure|beach|city|countryside|ski">
-						<header>
-							<span class="icon-down-circle-full"></span>
-							<h5>Pick the type of place</h5>
-						</header>
-					</div>
-					<div pp-inspiration-select id="season-select" class="select" data-options="spring|summer|winter|fall">
-						<header>
-							<span class="icon-down-circle-full"></span>
-							<h5>Pick a season</h5>
-						</header>
-					</div>
-				</div>
-				<button class="go-btn animated fadeIn" ng-click="submitInspiration()" ng-if="search.season && search.place">Go</button>
-			</div>
-		</div>
-	</section>
-	
-	<!-- how it works -->
-	
-	<section id="how-it-works">
-		<header>
-			<h4 data-animate="1">
-				Spotlight On
-				<span>Itineraries & City Guides</span>
-			</h4>
-		</header>
-		<div class="promoted-grid align-center" ng-controller="PromotedController">
-			<?php $i = 0; ?>
-			<?php if (isset($itineraries) && !empty($itineraries)): ?>
-				<?php foreach ($itineraries as $itinerary) { ?>
-					<a pp-promoted-itinerary id="<?php echo $itinerary; ?>" class="quick-entry fixed" data-animate="<?php echo $i+2; ?>"></a>
-				<?php $i++; } ?>
-			<?php endif; ?>
-			<?php if (isset($destinations) && !empty($destinations)): ?>
-				<?php foreach ($destinations as $destination) { ?>
-					<a href="<?php echo $destination['url']; ?>" class="quick-entry fixed destination" data-animate="<?php echo $i+2; ?>">
-						<figure>
-	                        <img src="<?php echo $destination['image']['url']; ?>" alt="<?php echo $destination['image']['text']; ?>"/>
-	                    </figure>
-	                    <footer>
-	                        <h4><?php echo $destination['title']; ?></h4>
-	                    </footer>
-					</a>
-				<?php $i++; } ?>
-			<?php endif; ?>			
-		</div>
-	</section>
-	
+
+    <!-- Search & Newsletter -->
+    <section id="search-and-newsletter" class="full">
+        <div class="wrapper">
+            <div id="find-your-trip" class="half">
+                <header>
+                    <h4>We'll Plan Your Perfect Trip</h4>
+                </header>
+                <div pp-inspiration-select id="place-select" class="select" data-options="adventure|beach|city|countryside|ski">
+                    <header>
+                        <span class="icon-down-circle-full"></span>
+                        <h5>Type of place</h5>
+                    </header>
+                </div>
+                <div pp-inspiration-select id="season-select" class="select" data-options="spring|summer|winter|fall">
+                    <header>
+                        <span class="icon-down-circle-full"></span>
+                        <h5>Season</h5>
+                    </header>
+                </div>
+            </div>
+            <div class="vertical-line-divider"></div>
+            <div id="newsletter-cta" class="half">
+                <form id="newsletter-form" name="newsletterForm" ng-show="type == 'newsletter'">
+                    <header>
+                        <h4>Sign Up to Our Newsletter</h4>
+                        <span ng-if="newsStatus == 'error'" class="animated fadeInDown">We're sorry, an error has occurred</span>
+                    </header>
+                    <div class="wrapper">
+                        <h3 ng-if="newsStatus == 'success'" class="animated fadeInDown">Thanks!</h3>
+                        <input name="user-email" class="full rounded" type="email" ng-if="newsStatus == ''|| newStatus == 'error'" placeholder="Your email address" ng-model="data.userEmail" required/>
+                    </div>
+                    <input type="submit" value="submit" ng-class="{disabled:!newsletterForm.$valid}" ng-click="!newsletterForm.$valid || regNewsletter()" ng-if="newsStatus == '' || newStatus == 'error'"/>
+                </form>
+            </div>
+        </div>
+    </section>
 	<!-- Blog -->
-	
 	<?php if (isset($new_travel_journal)) echo $new_travel_journal; ?>
+    <!-- Instagram -->
+    <section id="instagram-feed" class="full">
+        <div class="wrapper">
+            <header>
+                <h3>#getpassported</h3>
+            </header>
+            <div class="gallery-wrapper">
+                <ul>
+                    <li>
+                        <a href="">
+                            <img src="" alt=""/>
+                        </a>
+                    </li>
+                </ul>
+                <button rel="left">
+                    <svg width="40" height="80" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                        <use x="0" y="0" xlink:href="#left-arrow"/>
+                    </svg>
+                    <span></span>
+                </button>
+                <button rel="right">
+                    <svg width="40" height="80" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                        <use x="0" y="0" xlink:href="#right-arrow"/>
+                    </svg>
+                    <span></span>
+                </button>
+            </div>
+            <div id="plan-banner">
+                <figure>
+                    <img src="<?php echo drupal_get_path('theme','passported'); ?>/media/backgrounds/plan-your-next-trip.jpg" alt="Plan your next trip with Passported"/>
+                </figure>
+                <div class="vertical-wrapper">
+                    <h4>Plan your next trip with Passported</h4>
+                    <a href="https://go.passported.com/" class="rounded-btn white">Start your itinerary</a>
+                </div>
+            </div>
+        </div>
+    </section>
