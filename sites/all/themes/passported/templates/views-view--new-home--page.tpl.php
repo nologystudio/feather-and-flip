236608db7c3920b3f59bc4a75b09cf86e1f33ac6
Aday <aday@nologystudio.com>
2016-05-24 20:46:07 +0100

    Cosmetic Fixes
    + CSS Style
    + Newsletter trigger
    + HTML layout

--- sites/all/themes/passported/templates/views-view--new-home--page.tpl.php
+++ sites/all/themes/passported/templates/views-view--new-home--page.tpl.php
@@ -35,7 +35,7 @@
                     </header>
                     <div class="wrapper">
                         <h3 ng-if="newsStatus == 'success'" class="animated fadeInDown">Thanks!</h3>
-                        <input name="user-email" class="full rounded" type="email" ng-if="newsStatus == '' || newStatus == 'error'" placeholder="Your email address" ng-model="data.userEmail" required/>
+                        <input name="user-email" class="full rounded" type="email" ng-if="newsStatus != 'success'" placeholder="Your email address" ng-model="data.userEmail" required/>
                     </div>
                     <input type="submit" value="submit" ng-class="{disabled:!newsletterForm.$valid}" ng-click="!newsletterForm.$valid || regNewsletter()" ng-if="newsStatus == '' || newStatus == 'error'"/>
                 </form>
