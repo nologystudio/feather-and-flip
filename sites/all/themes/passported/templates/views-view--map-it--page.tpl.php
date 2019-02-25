4947305ff5561387f51a3b0d63bfeb06b28bc23a
Aday <aday@nologystudio.com>
2019-02-25 18:15:30 +0100

    + Redirect has been removed, use htaccess instead

--- sites/all/themes/passported/templates/views-view--map-it--page.tpl.php
+++ sites/all/themes/passported/templates/views-view--map-it--page.tpl.php
@@ -2,10 +2,6 @@
 
 	<!-- Map -->
 
-	<script>
-        window.location.href = 'https://www.passported.com/';
-    </script>
-	
 	<section id="map" class="city-guides" ng-controller="MapController">
 		<?php include 'map_content.php'; ?>
 	</section>
