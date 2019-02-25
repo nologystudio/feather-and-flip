5ad4f99c19f0843d00efe524aa3324c61e4fd034
Aday <aday@nologystudio.com>
2019-02-25 17:35:23 +0100

    + City Guide Redirect

--- sites/all/themes/passported/templates/views-view--city-guides--page.tpl.php
+++ sites/all/themes/passported/templates/views-view--city-guides--page.tpl.php
@@ -1,6 +1,10 @@
 
 
 	<!-- Map -->
+
+	<script>
+        window.location.href = 'https://www.passported.com/';
+    </script>
 	
 	<section id="map" class="single-destination" ng-controller="MapController" ng-init="cityGuideID = <?php echo arg(1); ?>;lat = <?php echo (isset($lat) ? $lat : ''); ?>;lon = <?php echo (isset($lon) ? $lon : ''); ?>">
 		<?php include 'map_content.php'; ?>
